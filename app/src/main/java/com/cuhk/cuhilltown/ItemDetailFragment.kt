22bd788ea935f245c37a57494dd9f26d081950c3
Leo Choi <choimankin@gmail.com>
2019-03-31 14:58:19 +0800

    Done playback

--- app/src/main/java/com/cuhk/cuhilltown/ItemDetailFragment.kt
+++ app/src/main/java/com/cuhk/cuhilltown/ItemDetailFragment.kt
@@ -1,10 +1,16 @@
 package com.cuhk.cuhilltown
 
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.support.annotation.AnyRes
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.VideoView
 import kotlinx.android.synthetic.main.activity_item_detail.*
 import kotlinx.android.synthetic.main.item_detail.view.*
 
@@ -16,10 +22,11 @@ import kotlinx.android.synthetic.main.item_detail.view.*
  */
 class ItemDetailFragment : Fragment() {
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
     private var item: Item? = null
+
+    private lateinit var rootView: View
+    private lateinit var videoView: VideoView
+    private lateinit var mediaCtrl: MediaController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,14 +48,53 @@ class ItemDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.item_detail, container, false)
+        rootView = inflater.inflate(R.layout.item_detail, container, false)
 
         // Show the dummy content as text in a TextView.
         item?.let {
-            rootView.item_detail.text = it.videoUrl
+            rootView.item_detail.text = item!!.videoUrl
+
+            videoView = rootView.my_video
+
+            mediaCtrl = MediaController(rootView.context)
+            mediaCtrl.setAnchorView(videoView)
+
+            videoView.setMediaController(mediaCtrl)
+
+            if (item!!.videoUrl == "") {
+                val uri = getUriFromDrawable(rootView.context, item!!.raw!!)
+                videoView.setVideoURI(uri)
+
+            } else {
+                videoView.setVideoURI(Uri.parse(item!!.videoUrl))
+            }
+
+            videoView.start()
         }
 
         return rootView
+    }
+
+    override fun onDestroyView(){
+        this.videoView.stopPlayback()
+
+        super.onDestroyView()
+    }
+
+
+    override fun onPause() {
+        this.videoView.pause()
+
+        super.onPause()
+    }
+
+    private fun getUriFromDrawable(context: Context, @AnyRes drawableId: Int): Uri {
+        return Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE +
+                    "://" + context.getResources().getResourcePackageName(drawableId)
+                    + '/'.toString() + context.getResources().getResourceTypeName(drawableId)
+                    + '/'.toString() + context.getResources().getResourceEntryName(drawableId)
+        )
     }
 
     companion object {
